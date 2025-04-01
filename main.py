3b0097f4c8787b8d5818ed0d9c1e2f3fe2defa32
wolderufael <wolderuphaelkassahun@gmail.com>
2025-04-01 14:00:26 +0300

    add tool

--- main.py
+++ main.py
@@ -44,3 +44,30 @@ async def fetch_url(url: str):
             return text
         except httpx.TimeoutException:
             return "Timeout error"
+
+
+@mcp.tool()  
+async def get_docs(query: str, library: str):
+  """
+  Search the latest docs for a given query and library.
+  Supports langchain, openai, and llama-index.
+
+  Args:
+    query: The query to search for (e.g. "Chroma DB")
+    library: The library to search in (e.g. "langchain")
+
+  Returns:
+    Text from the docs
+  """
+  if library not in docs_urls:
+    raise ValueError(f"Library {library} not supported by this tool")
+  
+  query = f"site:{docs_urls[library]} {query}"
+  results = await search_web(query)
+  if len(results["organic"]) == 0:
+    return "No results found"
+  
+  text = ""
+  for result in results["organic"]:
+    text += await fetch_url(result["link"])
+  return text
